8af190a1edf9e345f9df399542d52a10ca90463b
Dominique Ducommun <hello@do.min.icu>
2023-10-20 14:12:55 +0200

    Updating @definitions examples

--- src/@definitions.php
+++ src/@definitions.php
@@ -1,26 +1,92 @@
 <?php
 
-use App\Models\Company;
-
 return [
-    /*"atoms.button" => [
+    /* Simple example */
+    /*
+    "atoms.button" => [
+        "default" => [ # Variation with 'default' key is a special one and lets you define fallbacks
+            "slots" => [
+                "slot" => "Click me button" # Slot with 'slot' key is the default one
+            ],
+            "attributes" => [
+                "variation" => "primary",
+                "type" => "button",
+                "size" => "medium",
+            ]
+        ],
         "primary" => [
-            "label" => "Primary",
             "attributes" => [
                 "variation" => "primary",
             ]
         ],
         "secondary" => [
-            "label" => "Secondary",
             "attributes" => [
                 "variation" => "secondary",
             ]
         ],
-        "naked" => [
-            "label" => "Naked",
+        "link" => [
+            "slots" => [
+                "slot" => "Click me link", # Will override 'default.slots.slot'
+            ],
             "attributes" => [
-                "variation" => "naked",
+                "type" => "href", # Will override 'default.attributes.type'
+                "size" => "small", # Will override 'default.attributes.size'
+                "variation" => "primary",
             ]
         ]
-    ]*/
+    ],
+    */
+
+    /* More advanced example with slots, queries and assets */
+    /*
+    "cards.card" => [
+        "default" => [
+            "slots" => [
+                "heading" => view('components.cards.heading'),
+                "slot" => "This is the default slot content",
+                "footer" => view('components.cards.footer']),
+            ],
+            "assets" => [ # Will load those vite bundles in all variations of this component
+                "tailwind",
+                "alpine",
+            ],
+            "attributes" => [
+                "movie" => Movie::find(1) # You can pass queries to your component if you need to
+                "displayButton" => false,
+                "displayImage" => false,
+            ]
+        ],
+        "rich" => [
+            "label" => "Rich Card",
+            "attributes" => [
+                "movie" => Movie::find(2),
+                "displayButton" => true,
+                "displayImage" => true,
+            ]
+        ],
+        "animated" => [
+            "label" => "Animated",
+            "assets" => [
+                "animatedCard"
+            ],
+            "attributes" => [
+                "movie" => Movie::find(2),
+                "displayButton" => true,
+                "displayImage" => true,
+            ],
+        ]
+    ],
+    "grid" => [
+        "default" => [
+            "assets" => [
+                "grid",
+                "gridFilters",
+            ],
+            "attributes" => [
+                "movies" => Movies::where('category', 'comedy'),
+                "pagination" => true,
+            ]
+        ]
+    ]
+    */
 ];
